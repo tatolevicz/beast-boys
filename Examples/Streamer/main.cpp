df26469bcd22234c352f533850167acf97f9b800
Tato Levicz <leviczios@gmail.com>
2023-03-06 11:02:45 -0300

    added comments

--- Examples/Streamer/main.cpp
+++ Examples/Streamer/main.cpp
@@ -9,14 +9,18 @@ int main(){
     std::shared_ptr<bb::Streamer> streamer(new bb::Streamer());
 
     auto stream = streamer->openStream("localhost","1234","", false, [](bool success, const std::string& data){
-        if(success)
+        if(success) {
+            //Work with your streamed data here
             std::cout << data;
+        }
         else
-            std::cout << "Stream closed with error: " << data << "\n\n";
+            std::cout << "Stream closed with msg: " << data << "\n\n";
     });
 
     while(stream.lock()){
-
+        // Do other stuff while the data is coming in callback
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::cout << "\nWorking.. \n";
     }
 
     return 0;
