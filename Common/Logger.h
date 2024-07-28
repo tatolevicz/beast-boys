9040c641c9ad9ffe699fb80256fc9e6667e815ee
Tato Levicz <leviczios@gmail.com>
2024-07-28 13:17:08 -0300

    looger with number + default info

--- Common/Logger.h
+++ Common/Logger.h
@@ -2,27 +2,29 @@
 // Created by Arthur Motelevicz on 04/03/23.
 //
 
-#ifndef BINANCEBEAST_LOGGER_H
-#define BINANCEBEAST_LOGGER_H
+#ifndef BEASTBOYS_LOGGER_H
+#define BEASTBOYS_LOGGER_H
 
 #include <iostream>
 
 #define CHECK_ASIO_ERROR_(ec) \
     if(ec.value() != 0) { \
-        std::cerr << "Error: " << ec.value() \
-        << " at " << __FILE__ \
-        << "(" << __LINE__ << ")"            \
-        << " Message: " << ec.message() << "\n"; \
+        std::cerr << "Error Code: " << ec.value() \
+                  << " | Error Number: " << std::generic_category().default_error_condition(ec.value()).value() \
+                  << " | Error Message: " << ec.message()                                                       \
+                  << " | Default Error Message: " << ec.default_error_condition().message() \
+                  << " | File: " << __FILE__ << "(" << __LINE__ << ")\n"; \
         return; \
     }
 
 #define REPORT_ASIO_ERROR_(ec) \
     if(ec.value() != 0) { \
-        std::cerr << "Error: " << ec.value() \
-        << " at " << __FILE__ \
-        << "(" << __LINE__ << ")"            \
-        << " Message: " << ec.message() << "\n"; \
+        std::cerr << "Error Code: " << ec.value() \
+                  << " | Error Number: " << std::generic_category().default_error_condition(ec.value()).value() \
+                  << " | Error Message: " << ec.message()                                                       \
+                  << " | Default Error Message: " << ec.default_error_condition().message() \
+                  << " | File: " << __FILE__ << "(" << __LINE__ << ")\n"; \
     }
 
 
-#endif //BINANCEBEAST_LOGGER_H
+#endif //BEASTBOYS_LOGGER_H
