1c8ee0b1ebdbfe4ed0f22dbeae13765c07cd2759
Tato Levicz <leviczios@gmail.com>
2024-08-28 11:43:12 -0300

    update utils + fix generate cacert path

--- cmake/generate_cacert_data.cmake
+++ cmake/generate_cacert_data.cmake
@@ -2,10 +2,10 @@
 set(CACERT_URL "https://curl.se/ca/cacert.pem")
 
 # Define the output file name
-set(OUTPUT_FILE "${CMAKE_SOURCE_DIR}/Common/cacert_data.h")
+set(OUTPUT_FILE "${CMAKE_CURRENT_SOURCE_DIR}/Common/cacert_data.h")
 
 # Define the temporary file to store cacert.pem
-set(TEMP_FILE "${CMAKE_BINARY_DIR}/cacert.pem")
+set(TEMP_FILE "${CMAKE_CURRENT_BINARY_DIR}/cacert.pem")
 
 # Download the latest cacert.pem
 message(STATUS "Downloading cacert.pem from ${CACERT_URL}...")
